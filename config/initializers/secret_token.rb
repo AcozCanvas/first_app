205b1ffd047081b6c7248904a9c4fcf263ec4967
AcozCanvas <acostanzo@canvas.is>
2012-07-16 11:11:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a782f6b528ab5fb1c7209e94355632541fa232df3300796783f137d3c9f2f7b6ecbc4a15c82af2259001baa14aa4e9ac3d662a3e5f35becab70c4cd3d8deacee'
